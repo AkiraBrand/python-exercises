1ad8a196fd55920b6b860f583ce0bd0af243f5ce
Ruth Brand <ruthbrandsoprano@gmail.com>
2019-12-02 14:23:10 -0800

    adds string exercises

---
+++ strings.py
@@ -0,0 +1,52 @@
+def test_1(self):
+    name = "alice"
+    #call method on name to acheive expected output
+    actual = _______
+    expected = "Alice"
+    assert actual == expected
+
+def test_2(self):
+    name = "aLiCe"
+    #same thing as on line 2
+    actual = _______
+    expected = "ALICE"
+    assert actual == expected
+
+def test_3(self):
+    name = "AlIcE"
+    #same as above
+    actual = _______
+    expected = "alice"
+    assert actual == expected
+
+def test_4(self):
+    rhyme = "peter piper picked a peck of pickled peppers"
+    #''
+    actual = ______
+    expected = "sreppep dekcip fo kcep a dekcip repip retep"
+    assert actual == expected
+
+def test_5(self):
+    word = "ticking"
+    actual = ________
+    expected = "kicking"
+    assert actual == expected
+
+def test_6(self):
+    word = "ticking"
+    actual = _________
+    expected = "clocking"
+    assert actual == expected
+
+def test_7(self):
+    words = "five sleepy kittens"
+    actual = ________
+    expected = "fiv* sl**py kitt*ns"
+    assert actual == expected
+
+def test_8(self):
+    greeting = "Hello!!"
+    actual = ______
+    expected = "Hello!"
+    assert actual == expected
+    
